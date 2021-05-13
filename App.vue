b132ebc30c1359c5de1fb0b585d7358cbfbbb619
zhangqian531 <1467768259@qq.com>
2021-05-13 15:13:25 +0800

    第一版

---
+++ App.vue
@@ -0,0 +1,105 @@
+<script>
+	import Vue from 'vue';
+	import {mapState,mapMutations} from 'vuex';
+	import VerifyHelper from "@/common/verify.js";
+	export default {
+		onLaunch: function() {
+			this.GetUserInfo();
+		},
+		onShow: function() {
+			
+		},
+		onHide: function() {
+			
+		},
+		computed:{
+			...mapState(['hasLogin','userInfo','unionId','openId'])
+		},
+		methods:{
+			// 映射login状态到页面示例中，可以直接访问，实时获取Mutations的值
+			...mapMutations(['login']),
+			//获取微信数据
+			GetUserInfo() {
+				let _self=this;
+				uni.login({//获取code
+					provider: 'weixin',
+					success(loginRes) {
+						wx.getSetting({//判断是否授权
+							success(settingRes) {
+								if (settingRes.authSetting['scope.userInfo']) {//已授权直接获取用户信息
+									uni.getUserInfo({//获取用户信息
+										provider: 'weixin',
+										success(infoRes) {
+											_self.Decode(loginRes.code,infoRes.encryptedData,infoRes.iv);
+										},
+										fail(infoRes) {
+											uni.reLaunch({url:"/pages/login/login"});
+										}
+									});
+								}
+								else{//未授权等待用户主动授权
+									let openId = uni.getStorageSync('openId');
+									let unionId = uni.getStorageSync('unionId');
+									if(!VerifyHelper.IsNull(openId)){
+										_self.LoginByUnionID(unionId,openId);
+									}else{
+										uni.reLaunch({url:"/pages/login/login"});
+									}
+								}
+							}
+						});						
+					}
+				});
+			},
+			Decode(code,encryptedData,iv){
+				let _self=this;
+				let postData={
+					code:code,
+				}
+				_self.POST('/api/Pay/GetAppletsOpenidUnionid',postData,'',function(res){
+					uni.setStorageSync('unionId',res.Data.unionid);
+					uni.setStorageSync('openId',res.Data.openid);
+					_self.LoginByUnionID(res.Data.unionid,res.Data.openid);
+				},'applets','application/x-www-form-urlencoded')
+				
+			},
+			LoginByUnionID(unionId,openId){
+				let _self=this;
+				let mobile = uni.getStorageSync('mobile');
+				_self.POST('/api/Auth/LoginByUnionID',{unionId:unionId,openIdApplets:openId,mobile:mobile},'',function(result){
+					if(result.Status){
+						//console.log('result1',result)
+						_self.Get('/api/User/GetUserByUnionID',{},result.Data.accessToken,function(result2){
+							if(result2.Status){
+								result2.Data.accessToken=result.Data.accessToken;
+								result2.Data.refreshToken=result.Data.refreshToken;
+								result2.Data.OpenID=openId;
+								uni.setStorageSync('accessToken',result.Data.accessToken);
+								uni.setStorageSync('refreshToken',result.Data.refreshToken);
+								uni.setStorageSync('login',result2.Data);
+								uni.setStorageSync('fullname',result2.Data.FullName);
+								uni.setStorageSync('mobile',result2.Data.Mobile);
+								uni.setStorageSync('headurl',result2.Data.HeadUrl);
+								_self.login(result2.Data);
+								//uni.reLaunch({url:'/pages/index/index'});
+							}
+							//else uni.reLaunch({url:"/pages/login/login"});
+						},'biz');
+					}
+					//else uni.reLaunch({url:"/pages/login/login"});
+				},'auth','application/x-www-form-urlencoded');
+			},
+		}
+
+	}
+</script>
+
+<style>
+	@import url("/common/uni.css");
+	/*每个页面公共css */
+	*{margin: 0;padding: 0;}
+	.container-warp{min-height: 100vh;height:auto;}
+	.fl{float: left;}
+	.fr{float: right;}
+	.clr{clear: both;}
+</style>
---
+++ main.js
@@ -0,0 +1,207 @@
+import Vue from 'vue'
+import App from './App'
+import store from './store'
+
+Vue.config.productionTip = false
+
+App.mpType = 'app'
+
+//时间字符串转yyyy-mm-dd hh:mm:ss
+Vue.prototype.stringToDateTime = function(str,year="-",month="-",day="") {
+	if (str == null || str.length < 19) return "";
+	return str.substring(0,4)+year+str.substring(5,7)+month+str.substring(8,10)+day+" "+str.substring(11,19);
+}
+//时间字符串转yyyy-mm-dd
+Vue.prototype.stringToDate = function(str,year="-",month="-",day="") {
+	if (str == null || str.length < 10) return "";
+	return str.substring(0,4)+year+str.substring(5,7)+month+str.substring(8,10)+day;
+}
+//时间字符串转HH:mm
+Vue.prototype.stringToHour = function(str) {
+	if (str == null || str.length < 16) return "";
+	return str.substring(11,16);
+}
+
+//提示框
+Vue.prototype.showMsg= (title, duration=2000, mask=false, icon='none')=>{
+	if(Boolean(title) === false) return;
+	uni.showToast({title,duration,mask,icon});
+}
+
+Vue.prototype.POST=function(url,data,token,callback,type='biz',content='application/json'){
+	let _request = 'https://biz.zjxztc.com',t = this;
+	if(type=='auth') _request = 'https://auth.zjxztc.com';
+	else if(type=='passport') _request = 'https://passport.zjxztc.com';
+	else if(type=='applets') _request = 'https://applets.zjxztc.com'
+	else if(type=='repay') _request = 'https://repay.zjxztc.com'
+	uni.request({
+		url: _request+url,
+		method: 'POST',
+		header: {'content-type': content,'Authorization':'Bearer '+token},
+		data: data,
+		async:false,//同步属性设置为true 
+		complete:(res)=> {
+			uni.hideLoading();
+			if(res.statusCode==200)
+			{
+				callback(res.data);
+			}
+			else if(res.statusCode==401){
+				let accessToken = uni.getStorageSync('accessToken'),refreshToken = uni.getStorageSync('refreshToken');
+				if(!accessToken || !refreshToken){
+					  uni.reLaunch({
+							  url:'/pages/login/login.vue'
+							})
+				}else{
+					uni.request({
+						url: 'https://auth.zjxztc.com/api/Auth/RefreshToken',
+						method: 'POST',
+						header: {'content-type': 'application/json'},
+						data:  {
+								  "accessToken": accessToken,
+								  "refreshToken": refreshToken
+								},
+						async:false,//同步属性设置为true 
+						complete:(res1)=> {
+							if(res1.statusCode!=200||typeof(res1.data)!="object"||res1.data.Status == false){
+								  uni.reLaunch({
+										  url:'/pages/login/login.vue'
+										})
+							}
+							else{
+								accessToken = res1.data.Data.accessToken;
+								refreshToken = res1.data.Data.refreshToken;
+								t.userInfo.accessToken = accessToken;
+								t.userInfo.refreshToken = refreshToken;
+								uni.setStorageSync('accessToken',accessToken);
+								uni.setStorageSync('refreshToken',refreshToken);
+								uni.request({
+									url: _request+url,
+									method: 'POST',
+									header: {'content-type': content,'Authorization':'Bearer ' + accessToken},
+									data: data,
+									async:false,//同步属性设置为true 
+									complete:(res2)=>{
+										if(res2.statusCode==200){
+											callback(res2.data);
+										}
+										else{
+											this.showMsg("服务器异常!");
+										}
+									}
+								})
+							}
+						}
+					});
+				}
+			}
+			else if(res.statusCode==500){
+				this.showMsg("服务器错误，请重试!");
+			}
+			else{
+				console.log(res)
+				this.showMsg("未知错误，请重试!");
+			}
+		}
+	});
+}
+
+Vue.prototype.Get=function(url,data,token,callback,type='biz',content='application/json',async=false){
+	let _request = 'https://biz.zjxztc.com',t=this;
+	if(type=='auth') _request = 'https://auth.zjxztc.com';
+	else if(type=='passport') _request = 'https://passport.zjxztc.com';
+	else if(type=='applets') _request = 'https://applets.zjxztc.com'
+	else if(type=='repay') _request = 'https://repay.zjxztc.com'
+	uni.request({
+		url: _request+url,
+		method: 'Get',
+		header: {'content-type': content,'Authorization':'Bearer '+token},
+		data: data,
+		async:async,//同步属性设置为true 
+		complete:(res)=> {
+			uni.hideLoading();
+			if(res.statusCode==200)
+			{
+				callback(res.data);
+			}
+			else if(res.statusCode==401){
+				let accessToken = uni.getStorageSync('accessToken'),refreshToken = uni.getStorageSync('refreshToken');
+				if(!accessToken || !refreshToken){
+					  uni.reLaunch({
+							  url:'/pages/login/login.vue'
+							})
+				}else{
+					uni.request({
+						url: 'https://auth.zjxztc.com/api/Auth/RefreshToken',
+						method: 'POST',
+						header: {'content-type': 'application/json'},
+						data:  {
+								  "accessToken": accessToken,
+								  "refreshToken": refreshToken
+								},
+						async:async,//同步属性设置为true 
+						complete:(res1)=> {
+							if(res1.statusCode!=200||typeof(res1.data)!="object"||res1.data.Status == false){
+								  uni.reLaunch({
+										  url:'/pages/login/login.vue'
+										})
+							}
+							else{
+								accessToken = res1.data.Data.accessToken;
+								refreshToken = res1.data.Data.refreshToken;
+								t.userInfo.accessToken = accessToken;
+								t.userInfo.refreshToken = refreshToken;
+								uni.setStorageSync('accessToken',accessToken);
+								uni.setStorageSync('refreshToken',refreshToken);
+								uni.request({
+									url: _request+url,
+									method: 'Get',
+									header: {'content-type': content,'Authorization':'Bearer ' + accessToken},
+									data: data,
+									async:async,//同步属性设置为true 
+									complete:(res2)=>{
+										if(res2.statusCode==200)
+										{
+											callback(res2.data);
+										}
+										else{
+											this.showMsg("服务器异常!");
+										}
+									}
+								})
+							}
+						}
+					});
+				}
+			}
+			else{
+				this.showMsg("服务器异常!");
+			}
+		}
+	});
+}
+
+Vue.prototype.RefreshToken=function(accessToken,refreshToken,userInfo){
+	uni.request({
+		url: 'https://auth.zjxztc.com/api/Auth/RefreshToken',
+		method: 'POST',
+		header: {'content-type': 'application/json'},
+		data: data,
+		async:false,//同步属性设置为true 
+		complete:(res)=> {
+			if(res.statusCode!=200||typeof(res.data)!="object"){
+				
+			}
+			else{
+				userInfo.accessToken=res.data.Data.accessToken;
+				userInfo.refreshToken=res.data.Data.refreshToken;
+			}
+		}
+	});
+}
+
+const app = new Vue({
+	store,
+    ...App
+})
+app.$mount()
