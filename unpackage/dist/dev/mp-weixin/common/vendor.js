f344b5be639aa0a21955b212e6766cceca86dd4c
CWY <891955347@qq.com>
2021-05-19 14:48:54 +0800

    修复

--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -822,7 +822,7 @@ function initData(vueOptions, context) {
     try {
       data = data.call(context); // 支持 Vue.prototype 上挂的数据
     } catch (e) {
-      if (Object({"VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.warn('根据 Vue 的 data 函数初始化小程序 data 失败，请尽量确保 data 函数中不访问 vm 对象，否则可能影响首次数据渲染速度。', data);
       }
     }
@@ -7395,7 +7395,7 @@ function type(obj) {
 
 function flushCallbacks$1(vm) {
     if (vm.__next_tick_callbacks && vm.__next_tick_callbacks.length) {
-        if (Object({"VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+        if (Object({"NODE_ENV":"development","VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:flushCallbacks[' + vm.__next_tick_callbacks.length + ']');
@@ -7416,14 +7416,14 @@ function nextTick$1(vm, cb) {
     //1.nextTick 之前 已 setData 且 setData 还未回调完成
     //2.nextTick 之前存在 render watcher
     if (!vm.__next_tick_pending && !hasRenderWatcher(vm)) {
-        if(Object({"VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + vm._uid +
                 ']:nextVueTick');
         }
         return nextTick(cb, vm)
     }else{
-        if(Object({"VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG){
+        if(Object({"NODE_ENV":"development","VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG){
             var mpInstance$1 = vm.$scope;
             console.log('[' + (+new Date) + '][' + (mpInstance$1.is || mpInstance$1.route) + '][' + vm._uid +
                 ']:nextMPTick');
@@ -7509,7 +7509,7 @@ var patch = function(oldVnode, vnode) {
     });
     var diffData = this.$shouldDiffData === false ? data : diff(data, mpData);
     if (Object.keys(diffData).length) {
-      if (Object({"VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","NODE_ENV":"development","BASE_URL":"/"}).VUE_APP_DEBUG) {
+      if (Object({"NODE_ENV":"development","VUE_APP_NAME":"享指特快","VUE_APP_PLATFORM":"mp-weixin","BASE_URL":"/"}).VUE_APP_DEBUG) {
         console.log('[' + (+new Date) + '][' + (mpInstance.is || mpInstance.route) + '][' + this._uid +
           ']差量更新',
           JSON.stringify(diffData));
@@ -7947,9 +7947,9 @@ module.exports = g;
 
 /***/ }),
 /* 4 */
-/*!***************************************!*\
-  !*** D:/net享指特快代码/applets/pages.json ***!
-  \***************************************/
+/*!******************************************************************!*\
+  !*** C:/Users/admin/Desktop/新建文件夹 (3)/xztcnetapplets/pages.json ***!
+  \******************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -9072,9 +9072,9 @@ var index = {
 
 /***/ }),
 /* 9 */
-/*!*********************************************!*\
-  !*** D:/net享指特快代码/applets/common/verify.js ***!
-  \*********************************************/
+/*!************************************************************************!*\
+  !*** C:/Users/admin/Desktop/新建文件夹 (3)/xztcnetapplets/common/verify.js ***!
+  \************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9396,9 +9396,9 @@ function normalizeComponent (
 
 /***/ }),
 /* 13 */
-/*!*******************************************!*\
-  !*** D:/net享指特快代码/applets/store/index.js ***!
-  \*******************************************/
+/*!**********************************************************************!*\
+  !*** C:/Users/admin/Desktop/新建文件夹 (3)/xztcnetapplets/store/index.js ***!
+  \**********************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
@@ -9442,9 +9442,9 @@ store;exports.default = _default;
 /* 18 */,
 /* 19 */,
 /* 20 */
-/*!************************************************!*\
-  !*** D:/net享指特快代码/applets/common/utilofMap.js ***!
-  \************************************************/
+/*!***************************************************************************!*\
+  !*** C:/Users/admin/Desktop/新建文件夹 (3)/xztcnetapplets/common/utilofMap.js ***!
+  \***************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -9581,9 +9581,9 @@ module.exports = {
 
 /***/ }),
 /* 21 */
-/*!**********************************************!*\
-  !*** D:/net享指特快代码/applets/common/bmap-wx.js ***!
-  \**********************************************/
+/*!*************************************************************************!*\
+  !*** C:/Users/admin/Desktop/新建文件夹 (3)/xztcnetapplets/common/bmap-wx.js ***!
+  \*************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -10052,9 +10052,9 @@ module.exports.BMapWX = BMapWX;
 /* 28 */,
 /* 29 */,
 /* 30 */
-/*!********************************************************!*\
-  !*** D:/net享指特快代码/applets/common/verify.js?t=20201122 ***!
-  \********************************************************/
+/*!***********************************************************************************!*\
+  !*** C:/Users/admin/Desktop/新建文件夹 (3)/xztcnetapplets/common/verify.js?t=20201122 ***!
+  \***********************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
